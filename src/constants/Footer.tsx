75b121e2a33db13416c0ad325eed273f8ad596e9
Đỗ Văn Hải <haidv28062004@gmail.com>
2026-03-31 01:40:46 +0700

    style: update footer links and copyright information

--- src/constants/Footer.tsx
+++ src/constants/Footer.tsx
@@ -79,7 +79,7 @@ THÔNG TIN CHÚNG TÔI THU THẬP:
                                 </li>
                                 <li className="flex items-center gap-2 text-sm text-gray-400">
                                     <svg className="w-4 h-4 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" /></svg>
-                                    e-books.info.vn
+                                    landing.e-books.info.vn
                                 </li>
                             </ul>
                         </div>
